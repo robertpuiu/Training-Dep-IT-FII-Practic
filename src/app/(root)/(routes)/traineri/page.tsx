5901d54681a5a1010924821560ffbcd435b6835a
alexnec29 <alexandrunec29@gmail.com>
2024-12-07 16:14:36 +0200

    Rendering Images Trainers

--- src/app/(root)/(routes)/traineri/page.tsx
+++ src/app/(root)/(routes)/traineri/page.tsx
@@ -4,21 +4,35 @@ import type { FC } from "react";
 
 const TrainersPage: FC = async () => {
   let trainers = await fetch(process.env.NEXT_PUBLIC_APP_URL+"/api/traineri").then((res) => res.json());
-
+  console.log(trainers.map((trainer:any)=>{
+    return trainer.name.toLowerCase().replaceAll(" ", "-").replace("ă", "a").replace("î", "i").replace("â", "a").replace("ș", "s").replace("ț", "t");
+  }))
   return (
     <div> 
-      {trainers.map((trainer: any) => console.log(trainer))}
-      <Card>
-        <CardHeader>
-          <CardTitle>{/* Title me pls */}</CardTitle>
-          <CardContent>
-            <Image />
-            {/* no hentai pls */}
-          </CardContent>
-        </CardHeader>
-      </Card>
+      {trainers.map((trainer: any) => (
+          <Card key={trainer.id} className="p-4">
+            <CardContent>
+              <div className="flex flex-col items-center text-center">
+                <Image
+                  src={`/trainers/${trainer.name.toLowerCase().trim().split(" ").join("-").trim().replaceAll("ă", "a").replaceAll("î", "i").replaceAll("â", "a").replaceAll("ș", "s").replaceAll("ț", "t")}.png`}
+                  alt={`Picture of ${trainer.name}`}
+                  width={150}
+                  height={150}
+                  className="rounded-full"
+                />
+              </div>
+            </CardContent>
+            <CardHeader>
+                <CardTitle className="text-center text-lg font-bold mt-4">
+                  {trainer.name}
+                </CardTitle>
+              </CardHeader>
+          </Card>
+        ))}
     </div>
   );
 };
 
 export default TrainersPage;
+
+
