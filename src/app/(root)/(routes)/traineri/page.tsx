3a38ce1b3b6b3a83d7df859aba7653c8677d6461
Stoian Cristian <stoiancristian2002@gmail.com>
2024-12-07 15:48:11 +0200

    Map trainers

--- src/app/(root)/(routes)/traineri/page.tsx
+++ src/app/(root)/(routes)/traineri/page.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 import type { FC } from "react";
 
 const TrainersPage: FC = async () => {
-  let trainers = await fetch("/api/traineri").then((res) => res.json());
+  let trainers = await fetch(process.env.NEXT_PUBLIC_APP_URL+"/api/traineri").then((res) => res.json());
 
   return (
     <div> 
+      {trainers.map((trainer: any) => console.log(trainer))}
       <Card>
         <CardHeader>
           <CardTitle>{/* Title me pls */}</CardTitle>
