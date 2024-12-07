25998193069d2a62f4a3dc5e5f17a6848605f4eb
roberta34 <buzenchiroberta@gmail.com>
2024-12-07 15:39:34 +0200

    Parteneri fetch

--- src/app/(root)/(routes)/parteneri/page.tsx
+++ src/app/(root)/(routes)/parteneri/page.tsx
@@ -3,13 +3,18 @@ import Image from "next/image";
 import type { FC } from "react";
 
 const PartnersPage: FC = async () => {
+  const response=await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/parteneri`)
+  const traineri = await response.json();
+
+  console.log(traineri);
   return (
     <div>
       <Card>
         <CardHeader>
           <CardTitle>{/* Title me pls */}</CardTitle>
           <CardContent>
-            <Image />
+
+            <Image src="" alt=""/>
             {/* no hentai pls */}
           </CardContent>
         </CardHeader>
