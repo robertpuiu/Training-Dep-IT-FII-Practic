14e3c26a5db45a11571f88305061b0589afafe52
cosminciobanu18 <2jocker2.gr@gmail.com>
2024-12-07 15:51:05 +0200

    Update page.tsx

--- src/app/(root)/(routes)/parteneri/page.tsx
+++ src/app/(root)/(routes)/parteneri/page.tsx
@@ -3,18 +3,27 @@ import Image from "next/image";
 import type { FC } from "react";
 
 const PartnersPage: FC = async () => {
-  const response=await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/parteneri`)
-  const traineri = await response.json();
-
-  console.log(traineri);
+  function formatString(input) {
+    return input.replace(/\s+/g, "").toLowerCase();
+  }
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_URL}/api/parteneri`
+  );
+  if (!response.ok) {
+    throw new Error("Eroare");
+  }
+  const parteneri = await response.json();
+  parteneri.map((partener) => {
+    partener.imgPath = `./partneneri/${formatString(partener.name)}.png`;
+  });
+  console.log(parteneri);
   return (
     <div>
       <Card>
         <CardHeader>
           <CardTitle>{/* Title me pls */}</CardTitle>
           <CardContent>
-
-            <Image src="" alt=""/>
+            <Image src="" alt="" />
             {/* no hentai pls */}
           </CardContent>
         </CardHeader>
